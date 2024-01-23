db4ec550c7c5847dfb494093a93adeaf59c1d47a
akarakosee <132479583+akarakosee@users.noreply.github.com>
2024-01-23 15:50:43 +0300

    Add files via upload

---
+++ harfleri alfabetik sıralama.cpp
@@ -0,0 +1,42 @@
+﻿// harfleri alfabetik sıralama.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+
+	char dizi[40];
+	int sayac = 0;
+	int temp;
+	cout << "Lütfen Bir Kelime Giriniz (Hepsi Küçük Harf Olsun)" << endl;
+	cin >> dizi;
+	while (dizi[sayac] != '\0')
+	{
+		sayac++;
+	}
+	
+	int i = 0, j = 0;
+
+	while (i < sayac-1)
+	{
+		j = i + 1;
+		while (j < sayac)
+		 
+		{
+			if (dizi[i] > dizi[j])
+			{
+				temp = dizi[i];
+				dizi[i] = dizi[j];
+				dizi[j] = temp;
+			}
+			
+			j++;
+		}
+
+		i++;
+	}
+	cout<<	"Kelimeninin Yeni Hali:  "<< dizi << endl;
+}
+
